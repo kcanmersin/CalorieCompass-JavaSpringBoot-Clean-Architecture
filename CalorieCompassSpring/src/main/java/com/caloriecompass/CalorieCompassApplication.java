f68202572386721b1a0246a42a565082826c4e3e
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2025-04-07 18:55:56 +0300

    init

---
+++ CalorieCompassSpring/src/main/java/com/caloriecompass/CalorieCompassApplication.java
@@ -0,0 +1,13 @@
+package com.caloriecompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieCompassApplication.class, args);
+	}
+
+}
---
+++ CalorieCompassSpring/src/test/java/com/caloriecompass/CalorieCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caloriecompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
